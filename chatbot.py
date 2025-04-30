6ef3c1970892a6ba2b02e2a2680550f49024a5b3
Gajender <Iamsanju0707@gmail.com>
2025-04-30 15:04:47 +0530

    adding the some info

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.messages import HumanMessage,AIMessage
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
 import os
@@ -20,10 +21,48 @@ model=ChatGroq(model="gemma2-9b-it")
 
 ## now try the model first
 
-response=model.invoke([
-    HumanMessage("hey i'm sanju"),
-    AIMessage("remeber the details"),
-    HumanMessage("what is the my name ")
-])
+# response=model.invoke([
+#     HumanMessage("hey i'm sanju"),
+#     AIMessage("remeber the details"),
+#     HumanMessage("what is the my name ")
+# ])
 
-print(response.content)
+# print(response.content)
+store={}
+def get_chat_history(session_id:str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id]=ChatMessageHistory()
+    return store[session_id]
+
+
+
+runnable=RunnableWithMessageHistory(model,get_chat_history)
+config={"configurable":{"session_id":"user_1"}}
+
+# response = runnable.invoke(
+#     {"input": "Hello"},
+#     config={"configurable": {"session_id": "user123"}}
+# )
+
+
+
+# m2=runnable.invoke(
+#     [HumanMessage("Hey my name is sanju and mca student ")],
+#     config
+# )
+# print(m2.content)
+
+# m3=runnable.invoke(
+#     [HumanMessage(content="Hey what is my name and field of study?? ")],
+#     config
+# )
+
+# print(m3.content)
+
+
+m4=runnable.invoke(
+    [HumanMessage(content="Hey who i'm   ")],
+    {"configurable":{"session_id":"user_1"}}
+)
+
+print(m4.content)
